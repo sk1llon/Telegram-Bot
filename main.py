019d35b41512826b7ee4905ce9a5d0eeb9cb7f96
sk1llon <133895852+sk1llon@users.noreply.github.com>
2024-01-27 12:49:29 +0300

    Bot is ready, soups and hot dishes doesn`t work

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 import sqlite3
 from random import choice
 from telebot.types import (InlineKeyboardMarkup, InlineKeyboardButton,
-                           ReplyKeyboardMarkup, KeyboardButton)
+                           ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove)
 from config import BOT_TOKEN
 
 
@@ -17,9 +17,19 @@ with sqlite3.connect('telegram_bot.db', check_same_thread=False) as conn:
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-menu = {'Рыба': 1000, 'Мясо': 2000, 'Сок': 300}
 
-choose_answer = ['Прекрасный выбор!', 'Желаете что-нибудь ещё?', 'Замечательно!', 'Великолепно!']
+menu = {'Супы': {'Борщ со сметаной': 390, 'Суп лапша куриная с яйцом': 390},
+        'Горячие блюда': {'Пельмени домашние': 550, 'Филе морского окуня': 950, 'Запеченное филе куриной грудки': 690,
+                          'Котлеты куриные с картофельным пюре': 630},
+        'Горячие закуски': {'Креветки Темпура': 510, 'Куриные крылья': 510},
+        'Салаты': {'Салат "Цезарь" с курицей': 590, 'Салат "Цезарь" с тигровыми креветками': 930,
+                   'Салат "Дальневосточный"': 760, 'Салат "Варадеро': 610},
+        'Напитки': {'Морс клюквенный': 210, 'Боржоми': 250, 'Апельсиновый сок': 420, 'Яблочный сок': 420}}
+
+choose_answer = ['Прекрасный выбор!', 'Желаете что-нибудь ещё?', 'Замечательно!',
+                 'Великолепно!', 'Может быть что-нибудь ещё?']
+
+menu_path = None
 name = None
 table_number = None
 order_list = list()
@@ -32,8 +42,8 @@ def start(message):
     Приветствие и получение имени пользователя
     """
     msg = bot.send_message(message.chat.id,
-                           'Привет! Я бот, который поможет вам выбрать и заказать еду в нашем ресторане'
-                           '\n\nВведите ваше имя')
+                           'Привет! Я бот, который поможет вам выбрать и заказать еду в нашем ресторане\n\n'
+                           'Введите ваше имя')
     bot.register_next_step_handler(msg, get_name)
 
 
@@ -67,38 +77,85 @@ def get_table(message):
     """
     global table_number
     table_number = message.text
-    bot.send_message(message.chat.id, 'Что-нибудь желаете? Нажмите "Всё" когда закончите', reply_markup=gen_markup())
+    bot.send_message(message.chat.id, 'Наше меню',
+                     reply_markup=menu_type_markup())
 
 
-def gen_markup():
+def menu_type_markup():
     """
     Создание готовых ответов для пользователя (список блюд)
     """
     markup = InlineKeyboardMarkup()
-    for i_dish, i_price in menu.items():
-        dish = InlineKeyboardButton(resize_markup=True, text='{dish} - {price}'.format(
-            dish=i_dish, price=i_price), callback_data=i_dish)
+    for i_dish in menu.keys():
+        dish = InlineKeyboardButton(resize_markup=True, text=i_dish, callback_data=i_dish)
         markup.add(dish)
-    call_no = InlineKeyboardButton(resize_markup=True, text='Всё', callback_data='Всё')
-    markup.add(call_no)
     return markup
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    if call.data == 'Всё':
+    """
+    Обработчик call`ов
+    """
+    if call.data == 'Меню':
+        bot.edit_message_text('Меню', call.message.chat.id, call.message.id, reply_markup=menu_type_markup())
+    elif call.data == 'Всё':
         check_order(call)
+    elif call.data in menu.keys():
+        send_food_categories(call)
     else:
-        get_order(call)
         bot.send_message(call.message.chat.id, text=choice(choose_answer))
+        get_order(call)
+
+    
+def send_food_categories(call):
+    """
+    Получает категорию меню выбранную пользователем и выводит блюда данной категории
+    """
+    if call.data == 'Супы':
+        bot.edit_message_text('Супы', call.message.chat.id, call.message.id,
+                              reply_markup=order_markup(call.data))
+    elif call.data == 'Горячие блюда':
+        bot.edit_message_text('Горячие блюда', call.message.chat.id, call.message.id,
+                              reply_markup=order_markup(call.data))
+    elif call.data == 'Горячие закуски':
+        bot.edit_message_text('Горячие закуски', call.message.chat.id, call.message.id,
+                              reply_markup=order_markup(call.data))
+    elif call.data == 'Салаты':
+        bot.edit_message_text('Салаты', call.message.chat.id, call.message.id,
+                              reply_markup=order_markup(call.data))
+    elif call.data == 'Напитки':
+        bot.edit_message_text('Напитки', call.message.chat.id, call.message.id,
+                              reply_markup=order_markup(call.data))
+
+
+def order_markup(menu_type):
+    """
+    Inline Keyboard для заказа
+    """
+    global menu_path
+    markup = InlineKeyboardMarkup()
+    if menu_type in menu.keys():
+        menu_path = menu_type
+    for i_dish, i_price in menu[menu_path].items():
+        dish = InlineKeyboardButton(resize_markup=True, text='{dish} - {price}'.format(
+            dish=i_dish,
+            price=i_price
+        ), callback_data=i_dish)
+        markup.add(dish)
+    call_no = InlineKeyboardButton(text='Всё', callback_data='Всё')
+    menu_call = InlineKeyboardButton(text='Меню', callback_data='Меню')
+    markup.add(call_no, menu_call)
+    return markup
 
 
 def get_order(call):
-    global order_list
+    """
+    Составление заказа
+    """
+    global order_list, menu_path
     order_list.append(call.data)
-    if len(order_list) == 0:
-        bot.send_message(call.message.chat.id, 'Что-нибудь желаете? Нажмите "Всё" когда закончите',
-                         reply_markup=gen_markup())
+    bot.edit_message_text(call.message.chat.id, call.message.id, reply_markup=order_markup(menu_path))
 
 
 def check_order(call):
@@ -111,16 +168,15 @@ def check_order(call):
                                            'Номер стола - {table_number}\n'
                                            'Заказ - {order}'.format(table_number=table_number,
                                                                     order=order_str))
-    msg = bot.send_message(call.message.chat.id, 'Всё верно?', reply_markup=gen_markup_2())
+    msg = bot.send_message(call.message.chat.id, 'Всё верно?', reply_markup=yes_no_markup())
     bot.register_next_step_handler(msg, finish)
 
 
-def gen_markup_2():
+def yes_no_markup():
     """
     Создание готовых ответов для пользователя
     """
     markup = ReplyKeyboardMarkup()
-    markup.row_width = 2
     call_yes = KeyboardButton('Да')
     call_no = KeyboardButton('Нет')
     markup.add(call_yes, call_no)
@@ -133,7 +189,7 @@ def finish(message):
     """
     global name, table_number, order_list, order_str
     if message.text == 'Да':
-        bot.send_message(message.chat.id, 'Заказ будет готов в течение 20 минут')
+        bot.send_message(message.chat.id, 'Заказ будет готов в течение 20 минут', reply_markup=ReplyKeyboardRemove())
         insert_data(name, table_number, order_str)
         name = None
         table_number = None
@@ -146,11 +202,14 @@ def finish(message):
         get_name(message)
     else:
         msg = bot.send_message(message.chat.id, 'Я вас немного не понял. Вся ли информация верна?',
-                               reply_markup=gen_markup_2())
+                               reply_markup=yes_no_markup())
         bot.register_next_step_handler(msg, finish)
 
 
 def insert_data(u_name, u_table, u_order):
+    """
+    Заносим данные в базу
+    """
     cursor.execute('INSERT INTO users (name, table_num, order_dishes) VALUES (?, ?, ?)',
                    (u_name, u_table, u_order))
     conn.commit()
